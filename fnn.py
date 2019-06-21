5e94a9daa79a620a58dcddfb8dfa5154fb3587a1
Kyriakos Ioulianou <kyriakosioulianou@Kyriakoss-iMac.local>
2019-06-21 19:25:40 +0200

    kmeans

--- fnn.py
+++ fnn.py
@@ -26,7 +26,7 @@ w3 = w2; w4 = w1; b2 = b1
 for epoch in range(50):
     if (epoch < 1):
         w1 = 0.5; w2 = 0.5; b1 = 0.5
-        w3 = w2; w4 = w1; b2 = b1
+    w3 = w2; w4 = w1; b2 = b1
     
     for t in range(4):
         if (t == 0): x1 = 1; x2 = 1
@@ -35,10 +35,13 @@ for epoch in range(50):
         if (t == 3): x1 = 0; x2 = 1
         pred = t
         hidden_layer_y(epoch, x1, x2, w1, w2, w3, w4, b1, b2, pred, result)
-        
+    
+    print("epoch:",epoch,"optimization",round(train-sum(result)),"w1:",round(w1,4),"w2:",round(w2,4),"w3:",round(w3,4),"w4:",round(w4,4),"b1:",round(-b1,4),"b2:",round(b2,4))
     convergence = sum(result) - train
     if (convergence >= -0.0000000001): break
     else: w2 += 0.05; b1 = w2
-    print(convergence)
+    result[0] = 0; result[1] = 0; result[2] = 0; result[3] = 0
+    
+    #print(convergence)
 
 print("epoch: ",epoch, "w1: " , w1, "w2: ", w2, "b1: ", b1, "b2: ", b2)
