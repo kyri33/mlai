423f6ded177a6a635256b5dae314ac90bcf7f265
Kyriakos Ioulianou <kyriakos@Kyriakoss-MacBook-Air.local>
2020-01-14 13:56:08 +0200

    aw

--- maponya/env/env.py
+++ maponya/env/env.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class FXEnv(gym.Env):
 
-    def __init__(self, commission=0.0,
+    def __init__(self, spread=0.0,
             initial_balance=10000, look_back=60):
         
         super(FXEnv, self).__init__()
@@ -14,7 +14,7 @@ class FXEnv(gym.Env):
         self.group_by = fdata.DAY
         self.look_back = look_back
         self.initial_balance = initial_balance
-        self.commission = commission
+        self.spread = spread
         
         self.action_space = spaces.Discrete(7)
         self.observation_space = spaces.Box(low=0, high=1, shape=[self.look_back, 12])
@@ -49,11 +49,13 @@ class FXEnv(gym.Env):
         obs_df = scaler.transform(obs_df)
 
     def step(self, action):
+        action = action - 3
         current_price = random.uniform(
             self.data.loc[self.step, 'Open'],
             self.data.loc[self.step, 'Close']
         )
         current_position = self._take_action(action, current_price)
+        self.prev_action = action
         self.mins_left -= 1
         self.current_min += 1
         self.step += 1
@@ -61,7 +63,7 @@ class FXEnv(gym.Env):
             self.prev_price = current_price
         
         # TODO ADD COMMISSION AND SLIPPAGE
-        reward = (current_price - self.prev_price) * self.prev_position - abs(current_position - self.prev_position)
+        reward = (current_price - self.prev_price) * self.prev_position - abs(current_position - self.prev_position) * self.spread
         self.prev_position = current_position
 
         if self.mins_left == 0:
@@ -74,7 +76,6 @@ class FXEnv(gym.Env):
         return obs, reward, done, {}
 
     def _take_action(self, action, current_price):
-        action = action - 3
         bought = 0
         sold = 0
         shorted = 0
@@ -87,22 +88,56 @@ class FXEnv(gym.Env):
 
         if action > self.prev_action:
             if self.prev_action < 0:
-                covered, sales += self._cover(current_price, action - self.prev_action)
+                covered, cost = self._cover(current_price, action - self.prev_action)
+                action += self.prev_action
             if action > 0:
+                bought, cost = self._long(current_price, action - self.prev_action)
+        elif action < self.prev_action:
+            if self.prev_action > 0:
+                sold, sales = self._sell(current_price, action - self.prev_action)
+                action += self.prev_action
+            if action < 0:
+                shorted, sales = self._short(current_price, action - self.prev_action)
+        
+        current_position = self.prev_position - shorted + covered - sold + bought
+        self.balance = self.balance + sales - cost
+        self.net_worth = self.balance + self.prev_position * current_price
+        return current_position
 
-            
-    
-    def _cover(current_price, action_diff):
+    def _sell(self, current_price, action_diff):
         total = 0
         units = 0
-        if -action_diff < self.prev_action:
+        if -action_diff >= self.prev_action:
+            units = self.prev_position
+            total = self.prev_position * current_price
+        else:
+            total = action_diff * self.position_amount
+            units = total / current_price
+        return abs(units), abs(total)
+
+    def _cover(self, current_price, action_diff):
+        total = 0
+        units = 0
+        if -action_diff <= self.prev_action:
             units = self.prev_position
             total = self.prev_position * current_price
         else:
             total = self.position_amount * action_diff
             units = total / current_price
         
-        return abs(units), total
+        return abs(units), abs(total)
+    
+    def _long(self, current_price, action_diff):
+        total = action_diff * self.position_amount
+        units = total / current_price
+        return abs(units), abs(total)
+    
+    def _short(self, current_price, action_diff):
+        total = action_diff * self.position_amount
+        units = total / current_price
+        return abs(units), abs(total)
+
+
 
 env = FXEnv()
 env.reset()
